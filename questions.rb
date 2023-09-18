4401433859708029c504d1004630f634585eebde
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-09-18 00:27:11 -0400

    Initial commit, create questions.rb

---
+++ questions.rb
@@ -0,0 +1,17 @@
+class Question
+
+  def initialize
+    @num_1 = rand(1..20)
+    @num_2 = rand(1..20)
+    @solution = @num_1 + @num_2
+  end
+
+  def new_question
+    return "What does #{@num_1} plus #{@num_2} equal?"
+  end
+
+  def solution
+    return @solution
+  end
+
+end
